b1079da8528b30ee253a0772e3dc7c9046e8e181
may811204 <ju40268@gmail.com>
2018-04-20 17:41:41 -0500

    dm final

---
+++ Final/dmfinal/data.py
@@ -0,0 +1,29 @@
+def Data():
+    data = [
+        {
+            'player_name' : 'Kobe',
+            'action_type': ['Jump Shot', 'Layup Shot', 'Driving Layup Shot', 'Turnaround Jump Shot', 'Fadeaway Jump Shot', 'Running Jump Shot', 'Pullup Jump shot', 'Turnaround Fadeaway shot', 'Slam Dunk Shot', 'Reverse Layup Shot'],
+            'action_type_value' :[18880, 2567, 1978, 1057, 1048, 926, 476, 439, 411, 395],
+            'shot_x': [1,2,4],
+            'shot_y': [1,2,5],
+            # 'Alley Oop Dunk Shot' : {
+            #     'shot_x' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 28, 43, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 17, 31, 4, 17, -6, 24, -5, 0, 0, 4, 6, 13, -23, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, -6, 0, 0, 0, 0, 6],
+            #     'shot_y' : [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, -13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 11, 3, -16, 17, 20, 14, 17, 9, 3, 6, 7, 11, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 5, 0, 0, 0, 0]
+            # },
+            # 'Another Type' : {
+            #     'shot_x' : [1, 2, 3],
+            #     'shot_y' : [4,5,6]
+            # },
+            'distribution' : [['name1', [100,200,400], [100,200,400]], ['name2', [1,3,5], [1,3,5]]],
+            'other' : 'None'
+        },
+        {
+            'player_name': 'LeBrown',
+            'action_type': ['Jump Shot', 'Layup Shot', 'Driving Layup Shot', 'Turnaround Jump Shot',
+                            'Fadeaway Jump Shot', 'Running Jump Shot', 'Pullup Jump shot', 'Turnaround Fadeaway shot',
+                            'Slam Dunk Shot', 'Reverse Layup Shot'],
+            'action_type_value': [18880, 2567, 1978, 1057, 1048, 926, 476, 439, 411, 395],
+        }
+    ]
+
+    return data
