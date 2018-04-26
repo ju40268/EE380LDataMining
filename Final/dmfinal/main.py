02af40e884aa8c9e4dfdcff6bf1bdfaabf7b1c11
may811204 <ju40268@gmail.com>
2018-04-26 17:41:44 -0500

    more plot

--- Final/dmfinal/main.py
+++ Final/dmfinal/main.py
@@ -1,45 +1,45 @@
 from flask import Flask, render_template
-# from data import Data
-# from court import CourtShape
+from data import Data
+from court import CourtShape
 #
 app = Flask(__name__)
-# data = Data()
-# court_shapes = CourtShape()
-#
-# @app.route('/')
-# @app.route('/<string:chosen_name>')
-# # @app.route('/profile/<name>')
-# def profile(chosen_name='Kobe'):
-#     if not chosen_name: chosen_name = 'Kobe'
-#     for d in data:
-#         if d['player_name'] == chosen_name:
-#             data_piece = d
-#     return render_template('profile.html', player=chosen_name, data=data_piece)
-#
-#
-# # showing the distribution on the court
-# @app.route('/layout')
-# def distribution(chosen_name=None):
-#     if not chosen_name: chosen_name = 'Kobe'
-#     print(data)
-#     for d in data:
-#         if d['player_name'] == chosen_name:
-#             data_piece = d
-#
-#     return render_template('distribution.html', court=court_shapes, data=data_piece)
-#
-#
-# @app.route('/profile/trending')
-# def create():
-#     return render_template('trending.html')
-
+data = Data()
+court_shapes = CourtShape()
 
 @app.route('/')
-def main():
-    return render_template('stats.html')
-@app.route('/percent')
-def percent():
-    return  render_template('percent.html')
+@app.route('/<string:chosen_name>')
+# @app.route('/profile/<name>')
+def profile(chosen_name='Kobe'):
+    if not chosen_name: chosen_name = 'Kobe'
+    for d in data:
+        if d['player_name'] == chosen_name:
+            data_piece = d
+    return render_template('profile.html', player=chosen_name, data=data_piece)
+
+
+# showing the distribution on the court
+@app.route('/layout')
+def distribution(chosen_name=None):
+    if not chosen_name: chosen_name = 'Kobe'
+    print(data)
+    for d in data:
+        if d['player_name'] == chosen_name:
+            data_piece = d
+
+    return render_template('distribution.html', court=court_shapes, data=data_piece)
+
+
+@app.route('/trending')
+def create():
+    return render_template('trending.html')
+
+#
+# @app.route('/')
+# def main():
+#     return render_template('stats.html')
+# @app.route('/percent')
+# def percent():
+#     return  render_template('percent.html')
 
 if __name__ == "__main__":
     app.run()
